01968b0058f1d06167e7c064325e67c7addff59d
arjunhari004 <hariarjun658@gmail.com>
2026-09-11 12:08:11 +0530

    Functions

---
+++ Functions/script.js
@@ -0,0 +1,20 @@
+function greeting(){
+    console.log("Good Morning");
+    console.log("Attend the jail fast");
+    
+}
+greeting()
+
+function coffeeMachine(customer,coffeetype,price){
+    console.log("Type of coffee",coffeetype);
+    console.log(HotelTaj);
+    console.log("Boi Water");
+    console.log("Add milk");
+    console.log("Add Coffee Powder");
+    console.log("Add Sugar");
+    console.log("Pour coffee in cup");
+    console.log("Total Price Is:",price);
+    console.log("Prepared Coffee For:",customer);
+
+    
+} coffeeMachine("Rose","Cold Coffee",65)
