31ca8f0c5d451de329e73d98c46e39abdb78e8dd
arjunhari004 <hariarjun658@gmail.com>
2026-09-16 12:18:10 +0530

    Javascript  workoutqns

--- Nested fn/script.js
+++ Nested fn/script.js
@@ -1,86 +1,86 @@
-// function outer(){
-//     console.log("This is outer Function");
-//     function inner(){
-//         console.log("This is inner Function");
+function outer(){
+    console.log("This is outer Function");
+    function inner(){
+        console.log("This is inner Function");
         
-//     }
-//     inner()
-// }
-// outer()
+    }
+    inner()
+}
+outer()
 
 
 
-// function outer(){
-//     let branch="Kochi"
-//     function inner(){
-//         console.log(branch);
-
-        
-//     }inner()
-// }
-// outer()
-
-
-
-// function f1(){
-//     function f2(){
-//         return "Hello This is kalam"
-//     }
-//     let res = f2()
-//         console.log(res);
-//         return res;
+function outer(){
+    let branch="Kochi"
+    function inner(){
+        console.log(branch);
 
         
-// }let x=f1()
-// console.log(x);
+    }inner()
+}
+outer()
 
 
 
+function f1(){
+    function f2(){
+        return "Hello This is kalam"
+    }
+    let res = f2()
+        console.log(res);
+        return res;
 
-
-// function calculateSalary(sal){
-//     function calculateBonus(){
-//         let Bonus= sal*10/100;
-//         // console.log(Bonus);
-//         return Bonus
         
-//     }
-//     let res=calculateBonus()
-//         // console.log(res);
-//         return res+sal //5000+50000=55000
+}let x=f1()
+console.log(x);
+
+
+
+
+
+function calculateSalary(sal){
+    function calculateBonus(){
+        let Bonus= sal*10/100;
+        // console.log(Bonus);
+        return Bonus
         
-// }let totalSalary=calculateSalary(50000)
-// console.log(totalSalary);
+    }
+    let res=calculateBonus()
+        // console.log(res);
+        return res+sal //5000+50000=55000
+        
+}let totalSalary=calculateSalary(50000)
+console.log(totalSalary);
 
 
 
-// const calculateBill = (units)=>{
-//     let bill;
-//     if(units <=100){
-//         bill = units *2;
-//       }else{
-//         bill = units *5;
-//       }  
-//     console.log("Electricity Bill:",bill);
+const calculateBill = (units)=>{
+    let bill;
+    if(units <=100){
+        bill = units *2;
+      }else{
+        bill = units *5;
+      }  
+    console.log("Electricity Bill:",bill);
     
-// };
-// calculateBill(80)
+};
+calculateBill(80)
 
 
 
 
 
-// const calculateBonus = (salary) =>{
-//     let bonus;
-//     if(salary>=30000){
-//         bonus = salary *0.10;
-//     }else{
-//         bonus = salary *0.05;
-//     }
-//     console.log("Bonus:",bonus);
+const calculateBonus = (salary) =>{
+    let bonus;
+    if(salary>=30000){
+        bonus = salary *0.10;
+    }else{
+        bonus = salary *0.05;
+    }
+    console.log("Bonus:",bonus);
     
-// };
-// calculateBonus(40000)
+};
+calculateBonus(40000)
 
 
 
