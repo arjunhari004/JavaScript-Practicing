27a23162a7442740f4195af17cb587a2becba5b4
arjunhari004 <hariarjun658@gmail.com>
2026-09-16 12:05:17 +0530

    Javascript  workoutqns

--- Nested fn/script.js
+++ Nested fn/script.js
@@ -1,35 +1,54 @@
-function outer(){
-    console.log("This is outer Function");
-    function inner(){
-        console.log("This is inner Function");
+// function outer(){
+//     console.log("This is outer Function");
+//     function inner(){
+//         console.log("This is inner Function");
+        
+//     }
+//     inner()
+// }
+// outer()
+
+
+
+// function outer(){
+//     let branch="Kochi"
+//     function inner(){
+//         console.log(branch);
+
+        
+//     }inner()
+// }
+// outer()
+
+
+
+// function f1(){
+//     function f2(){
+//         return "Hello This is kalam"
+//     }
+//     let res = f2()
+//         console.log(res);
+//         return res;
+
+        
+// }let x=f1()
+// console.log(x);
+
+
+
+
+
+function calculateSalary(sal){
+    function calculateBonus(){
+        let Bonus= sal*10/100;
+        // console.log(Bonus);
+        return Bonus
         
     }
-    inner()
-}
-outer()
-
-
-
-function outer(){
-    let branch="Kochi"
-    function inner(){
-        console.log(branch);
-
+    let res=calculateBonus()
+        // console.log(res);
+        return res+sal //5000+50000=55000
         
-    }inner()
-}
-outer()
+}let totalSalary=calculateSalary(50000)
+console.log(totalSalary);
 
-
-
-function f1(){
-    function f2(){
-        return "Hello This is kalam"
-    }
-    let res = f2()
-        console.log(res);
-        return res;
-
-        
-}let x=f1()
-console.log(x);
