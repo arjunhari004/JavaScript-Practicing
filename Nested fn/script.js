60c92fb56c02e8d0a8dfdd5c8eab6562e8c53eb7
arjunhari004 <hariarjun658@gmail.com>
2026-09-16 12:17:52 +0530

    Javascript  workoutqns

--- Nested fn/script.js
+++ Nested fn/script.js
@@ -38,17 +38,60 @@
 
 
 
-function calculateSalary(sal){
-    function calculateBonus(){
-        let Bonus= sal*10/100;
-        // console.log(Bonus);
-        return Bonus
+// function calculateSalary(sal){
+//     function calculateBonus(){
+//         let Bonus= sal*10/100;
+//         // console.log(Bonus);
+//         return Bonus
         
-    }
-    let res=calculateBonus()
-        // console.log(res);
-        return res+sal //5000+50000=55000
+//     }
+//     let res=calculateBonus()
+//         // console.log(res);
+//         return res+sal //5000+50000=55000
         
-}let totalSalary=calculateSalary(50000)
-console.log(totalSalary);
+// }let totalSalary=calculateSalary(50000)
+// console.log(totalSalary);
 
+
+
+// const calculateBill = (units)=>{
+//     let bill;
+//     if(units <=100){
+//         bill = units *2;
+//       }else{
+//         bill = units *5;
+//       }  
+//     console.log("Electricity Bill:",bill);
+    
+// };
+// calculateBill(80)
+
+
+
+
+
+// const calculateBonus = (salary) =>{
+//     let bonus;
+//     if(salary>=30000){
+//         bonus = salary *0.10;
+//     }else{
+//         bonus = salary *0.05;
+//     }
+//     console.log("Bonus:",bonus);
+    
+// };
+// calculateBonus(40000)
+
+
+
+const ticketPrice = (age) =>{
+    let price;
+    if (age<12){
+        price=100;
+    }else{
+        price =200;
+    }
+    console.log("Ticket Prices",price);
+    
+};
+ticketPrice(10);
