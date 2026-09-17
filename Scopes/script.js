59b7cac9ae898bf210d176a68c84136cd1a4cffd
arjunhari004 <hariarjun658@gmail.com>
2026-09-17 12:07:01 +0530

    Javascript  workoutqns

--- Scopes/script.js
+++ Scopes/script.js
@@ -23,3 +23,46 @@ if(true){
 console.log("Inside Block",a);
 console.log("Inside Block",b);
 console.log("Inside Block",c);
+
+
+
+//FUNCTION SCOPE
+
+
+function mahaGuru(){
+    var user1 = "rose";
+    let user2 = "Arjun";
+    const user3 = "Abhii";
+
+    console.log("Victim1",user1);
+    console.log("Victim2",user2);
+    console.log("Victim3",user3);
+    
+}
+mahaGuru();
+console.log("Victim1",user1);
+console.log("Victim2",user2);
+console.log("Victim3",user3);
+    
+
+
+//Block Scope
+
+
+if("Qspiders"){
+    var isBatteriesAvailable=false;
+    let studentsCount=700;
+    const branch="Kochi";
+
+    console.log("Is Batteries are available:",isBatteriesAvailable);
+    console.log("Total no of victims:",studentsCount);
+    console.log("Branch Name:",branch);
+    
+    
+    
+}
+console.log("is batteries are available:",isBatteriesAvailable);
+console.log("Branch Name:",branch);
+console.log("Total no of victims",studentsCount);
+
+
