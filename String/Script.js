507b1d6ebf4c0bb931ae80f0b5520c76ab008fac
arjunhari004 <hariarjun658@gmail.com>
2026-09-22 11:46:30 +0530

    Javascript  workoutqns

--- String/Script.js
+++ String/Script.js
@@ -27,17 +27,29 @@
 
 //HOw to access String Characters
 
-let name="Arjun";
-console.log(name);
-console.log(name[0]);//accessing
-console.log(name[5]);//accessing
-console.log(name[7]);//Undefined-Output
-console.log(name[-1]);//Undefined
-name[0]="b"//Cant change using index position
-name="belbert"//re-assigning
-console.log(name);
+// let name="Arjun";
+// console.log(name);
+// console.log(name[0]);//accessing
+// console.log(name[5]);//accessing
+// console.log(name[7]);//Undefined-Output
+// console.log(name[-1]);//Undefined
+// name[0]="b"//Cant change using index position
+// name="belbert"//re-assigning
+// console.log(name);
 
 
+
+//String-Interpolation->inserting variables or expressions inside string
+
+let name="Karthik";
+let age=23;
+console.log("my name is",name,"and i am ",age,"years old");
+console.log(`my name is ${name}and i am ${age}years old`);
+
+
+let a=10;
+let b=20;
+console.log(`sum of ${a} and ${b} is ${a+b}`);
 
 
 
