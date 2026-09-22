8cc06639f224e70d0a6fce68ddc0ded5c87f4615
arjunhari004 <hariarjun658@gmail.com>
2026-09-22 11:43:01 +0530

    Javascript  workoutqns

--- String/Script.js
+++ String/Script.js
@@ -1,35 +1,44 @@
-//Strings-sequence of characters used to represent text data.
-//using Single Quotes
+// //Strings-sequence of characters used to represent text data.
+// //using Single Quotes
 
-let city='Kochi';
-console.log("Using singke quote",city);
-
-
-//Using Double Quote
-
-let state="Kerala";
-console.log("Using Double Quote",state);
+// let city='Kochi';
+// console.log("Using singke quote",city);
 
 
-let message="Its Javascript class";
-console.log(message);
+// //Using Double Quote
+
+// let state="Kerala";
+// console.log("Using Double Quote",state);
 
 
-let information='We have two days leave for "Navarathri"';
-console.log(information);
+// let message="Its Javascript class";
+// console.log(message);
 
-//using Backtick(template literal)
 
-let info=`we have e days
-holiday`
-console.log(info);
+// let information='We have two days leave for "Navarathri"';
+// console.log(information);
+
+// //using Backtick(template literal)
+
+// let info=`we have e days
+// holiday`
+// console.log(info);
 
 
 //HOw to access String Characters
 
 let name="Arjun";
 console.log(name);
-console.log(name[0]);
+console.log(name[0]);//accessing
+console.log(name[5]);//accessing
+console.log(name[7]);//Undefined-Output
+console.log(name[-1]);//Undefined
+name[0]="b"//Cant change using index position
+name="belbert"//re-assigning
+console.log(name);
+
+
+
 
 
 
