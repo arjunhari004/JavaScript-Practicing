b2de2664360879eb65277d6840ce88267a6ec6c2
arjunhari004 <hariarjun658@gmail.com>
2026-09-25 11:58:13 +0530

    Javascript  workoutqns

--- Smethod/script.js
+++ Smethod/script.js
@@ -2,6 +2,6 @@
 
 let trainer="KALAM";
 // let a=trainer.substring(1,4)
-// let a=trainer.substring(4,2)
+// let a=trainer.substring(4,2)//here in substring it swaps the higher and lower values
 let a=trainer.substring(-3)
 console.log(a);
